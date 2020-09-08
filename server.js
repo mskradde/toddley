fa914e83b4eb2bb82eafc320683ffaf0a05b08dd
mskradde <skradde.marco@gmail.com>
2020-09-08 09:55:24 +0200

    Modify json-server

--- server.js
+++ server.js
@@ -1,6 +1,9 @@
 const express = require("express");
 const app = express();
 const path = require("path");
+const jsonServer = require("json-server");
+const router = jsonServer.router("db.json");
+const middlewares = jsonServer.defaults();
 
 const port = process.env.PORT || 3001;
 app.use(express.static(path.join(__dirname, "client/build")));
@@ -8,6 +11,15 @@ app.use(
   "/storybook",
   express.static(path.join(__dirname, "client/storybook-static"))
 );
+
+app.use(
+  jsonServer.rewriter({
+    "/api/*": "/$1",
+  })
+);
+app.use(router);
+app.use(middlewares);
+
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`);
 });

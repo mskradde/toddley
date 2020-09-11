c234b1f4f64f0d7e281f47730c002943badf39ba
mskradde <skradde.marco@gmail.com>
2020-09-11 16:23:02 +0200

    Delete starter svgs and remove some starter code

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,4 @@
 import React from "react";
-import logo from "./logo.svg";
 import "./App.css";
 // import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 
@@ -7,7 +6,6 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit this <code>src/App.js</code> and save to reload.
         </p>

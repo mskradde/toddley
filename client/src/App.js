f55fbf7b65572e570111d46bcd7acffed995a3ec
mskradde <skradde.marco@gmail.com>
2020-09-11 16:41:54 +0200

    Delete introduction.stories

--- client/src/App.js
+++ client/src/App.js
@@ -5,19 +5,7 @@ import "./App.css";
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <p>
-          Edit this <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Toddly - Chat App</h1>
     </div>
   );
 }

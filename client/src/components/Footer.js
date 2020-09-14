ac4090edcb3af50b377d9740f3fefc620ce63f18
mskradde <skradde.marco@gmail.com>
2020-09-14 13:42:17 +0200

    Add footer.js and footer.stories

---
+++ client/src/components/Footer.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Footer(props) {
+  return (
+    <div>
+      <h1>Footer</h1>
+    </div>
+  );
+}
+
+export default Footer;
---
+++ client/src/stories/Footer.stories.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+import Footer from "../components/Footer";
+
+export default {
+  title: "Footer",
+  component: Footer,
+};
+
+const Template = () => <Footer />;
+
+export const Mainfooter = Template.bind({});

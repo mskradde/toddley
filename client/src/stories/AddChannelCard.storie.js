e99b0858b375d90fd3e771633cf415440107193b
mskradde <skradde.marco@gmail.com>
2020-09-18 15:19:36 +0200

    Add AddChannelCard.js and AddChannelCard.stories.js

---
+++ client/src/stories/AddChannelCard.storie.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+import AddChannelCard from "../components/AddChannelCard";
+
+export default {
+  title: "AddChannelCard",
+  component: AddChannelCard,
+};
+
+const Template = () => <AddChannelCard />;
+
+export const AddChannel = Template.bind({});

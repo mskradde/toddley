bf785ac2cefef8dc8b89ff094b13793f586be8ab
mskradde <skradde.marco@gmail.com>
2020-09-21 11:54:53 +0200

    Add styling to AddChannelCard

--- client/src/components/AddChannelCard.js
+++ client/src/components/AddChannelCard.js
@@ -1,7 +1,38 @@
 import React from "react";
+import styled from "@emotion/styled";
+import PlusIcon from "../assets/plus-icon.svg";
 
-function AddChannelCard(props) {
-  return <div></div>;
+const Card = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  border-radius: 10%;
+  width: 50vw;
+  height: 50vw;
+  background-color: #ffffff;
+  padding: 5%;
+`;
+
+const Circle = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 100%;
+  height: 100%;
+  background-color: #25386426;
+  border-radius: 100%;
+`;
+
+function AddChannelCard() {
+  return (
+    <>
+      <Card>
+        <Circle>
+          <img src={PlusIcon} alt="Channel hinzufügen" />
+        </Circle>
+      </Card>
+    </>
+  );
 }
 
 export default AddChannelCard;

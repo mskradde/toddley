66c2000b0192353e3ae99b7537282611a557717d
mskradde <skradde.marco@gmail.com>
2020-09-24 11:55:12 +0200

    AddChannel

--- client/src/pages/ChannelScreen.js
+++ client/src/pages/ChannelScreen.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import Footer from "../components/Footer";
 import Header from "../components/Header";
 import ChannelCard from "../components/ChannelCard";
@@ -22,19 +22,48 @@ const Main = styled.div`
     padding: 0.5em;
   }
 `;
+
 export function ChannelScreen() {
+  const [channels, setChannels] = useState([
+    {
+      title: "Regenbogen-Gruppe",
+      imgSrc:
+        "https://images.unsplash.com/photo-1541692641319-981cc79ee10a?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60",
+    },
+    {
+      title: "Schmetterlings-Gruppe",
+      imgSrc:
+        "https://images.unsplash.com/photo-1541692641319-981cc79ee10a?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60",
+    },
+  ]);
   return (
     <Router>
       <Container>
-        <Header></Header>
+        <Header />
         <Main>
           <ChannelCard
             title="Neuer Channel"
             imgSrc={PlusIcon}
-            onClick={() => alert("Hallo")}
+            onClick={() =>
+              setChannels([
+                ...channels,
+                {
+                  title: "Schmetterlings-Gruppe",
+                  imgSrc:
+                    "https://images.unsplash.com/photo-1541692641319-981cc79ee10a?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60",
+                },
+              ])
+            }
           />
+          {channels.map((channel) => (
+            <ChannelCard
+              key={channel.title}
+              title={channel.title}
+              imgSrc={channel.imgSrc}
+            />
+          ))}
         </Main>
-        <Footer></Footer>
+        <Footer />
       </Container>
     </Router>
   );

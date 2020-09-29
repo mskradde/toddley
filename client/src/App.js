2a64e0b54df9f40c4d288086fbd4acd7304e8605
mskradde <skradde.marco@gmail.com>
2020-09-29 10:41:01 +0200

    Remove Footer in App.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,6 @@
 import React from "react";
 import GlobalStyles from "./GlobalStyles";
 import { BrowserRouter as Router } from "react-router-dom";
-import Footer from "./components/Footer";
 import ChannelScreen from "./pages/ChannelScreen";
 
 function App() {
@@ -11,7 +10,6 @@ function App() {
 
       <Router>
         <ChannelScreen />
-        <Footer />
       </Router>
     </>
   );

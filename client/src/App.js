41ff007fda9eeb76384288c9aeeca791735b9694
mskradde <skradde.marco@gmail.com>
2020-09-30 16:14:17 +0200

    Add Routes to App.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,15 +1,26 @@
 import React from "react";
 import GlobalStyles from "./GlobalStyles";
-import { BrowserRouter as Router } from "react-router-dom";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import ChannelScreen from "./pages/ChannelScreen";
+import RegistrationScreen from "./pages/RegistrationScreen";
+import LoginScreen from "./pages/LoginScreen";
 
 function App() {
   return (
     <>
       <GlobalStyles />
-
       <Router>
-        <ChannelScreen />
+        <Switch>
+          <Route path="/register">
+            <RegistrationScreen />
+          </Route>
+          <Route exact path="/">
+            <ChannelScreen />
+          </Route>
+          <Route path="/login">
+            <LoginScreen />
+          </Route>
+        </Switch>
       </Router>
     </>
   );
